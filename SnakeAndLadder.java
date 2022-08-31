4888ab1d8ac09570a27d1ecdc0463b5ba2679a78
Saikat7827 <saikatchakraborty957.sc@gmail.com>
2022-08-31 15:29:07 +0530

    Initial position of player

---
+++ SnakeAndLadder.java
@@ -0,0 +1,9 @@
+package com.bl.Assignment;
+
+public class SnakeAndLadder {
+	public static void main(String[] args) {
+		int position = 0;
+		System.out.println("Initial position of player starts from " +position);
+	}
+
+}
