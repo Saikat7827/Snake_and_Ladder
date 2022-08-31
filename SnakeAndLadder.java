55ab962d23fa7e796e3b275af6161c169d62166b
Saikat7827 <saikatchakraborty957.sc@gmail.com>
2022-08-31 16:19:09 +0530

    Random Dice Number

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,9 +1,15 @@
 package com.bl.Assignment;
 
+import java.util.Random;
+
 public class SnakeAndLadder {
 	public static void main(String[] args) {
 		int position = 0;
-		System.out.println("Initial position of player starts from " +position);
+		System.out.println("Initial position of the player " +position);
+		Random ran = new Random();
+		int dice = ran.nextInt(6);
+		System.out.println("Dice Outcome" +dice);
+		
 	}
 
 }
