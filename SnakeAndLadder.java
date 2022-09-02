7147f90099ae7b64bf2ada222db0e4ff657f45de
Saikat7827 <saikatchakraborty957.sc@gmail.com>
2022-09-02 20:08:11 +0530

    Snake and Ladder

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -3,13 +3,40 @@ package com.bl.Assignment;
 import java.util.Random;
 
 public class SnakeAndLadder {
+	static int Snake = 2;
+	static int Ladder = 1;
+	static int Noplay = 0;
+	
 	public static void main(String[] args) {
 		int position = 0;
-		System.out.println("Initial position of the player " +position);
 		Random ran = new Random();
-		int dice = ran.nextInt(6);
-		System.out.println("Dice Outcome" +dice);
+		int dice = ran.nextInt(6)+1;
+		System.out.println("Dice Outcome : "+dice);
+		System.out.println("dice: "+dice);
+		
+		int optionCheck = ran.nextInt(3);
+		System.out.println("optionCheck: "+optionCheck);
+		
+		if (optionCheck == Ladder)
+		{
+			System.out.println("Ladder");
+			position = position + dice;
+			
+		}
+		
+		else if (optionCheck == Snake && position > dice)
+		{
+			System.out.println("Snake");
+			position = position - dice;
+			
+		}
+		else {
+			System.out.println("No Play");
+			
+		}
+		System.out.println("position: "+position);
+		
 		
 	}
-
+	
 }
