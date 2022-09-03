b0d3aba003f3d1da71482c9a091b996e3c7ec979
Saikat7827 <saikatchakraborty957.sc@gmail.com>
2022-09-03 23:28:12 +0530

    Till winning Pos 100

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -3,40 +3,67 @@ package com.bl.Assignment;
 import java.util.Random;
 
 public class SnakeAndLadder {
-	static int Snake = 2;
-	static int Ladder = 1;
-	static int Noplay = 0;
-	
-	public static void main(String[] args) {
+	static final int Snake = 2;
+	static final int ladder = 1;
+	static final int Ladder = 1;
+	static final int Noplay = 0;
+
+	public static void main(String[] args) 
+	{
 		int position = 0;
 		Random ran = new Random();
 		int dice = ran.nextInt(6)+1;
-		System.out.println("Dice Outcome : "+dice);
 		System.out.println("dice: "+dice);
-		
+		System.out.println("Initial Position is "+position);
+
 		int optionCheck = ran.nextInt(3);
 		System.out.println("optionCheck: "+optionCheck);
-		
-		if (optionCheck == Ladder)
+
+		if (optionCheck == ladder) 
+		while (position<100) 
 		{
 			System.out.println("Ladder");
 			position = position + dice;
-			
 		}
-		
-		else if (optionCheck == Snake && position > dice)
+
+		else if (optionCheck == Snake && position > dice) 
 		{
 			System.out.println("Snake");
 			position = position - dice;
-			
 		}
 		else {
 			System.out.println("No Play");
-			
+			Random ran1 = new Random();
+
+			int dice1 = ran1.nextInt(6)+1;
+			System.out.println("Dice Outcome "+dice1);
+
+			int optionCheck1 = ran1.nextInt(3);
+			System.out.println("optionCheck: "+optionCheck1);
+
+
+			if (optionCheck1 == Ladder) 
+			{
+				System.out.println("Ladder");
+				position = position + dice1;
+			}
+			else if (optionCheck1 == Snake) 
+			{
+				System.out.println("Snake");
+				position = position - dice1;
+			}
+			else 
+			{
+				System.out.println("NoPlay");
+			}
+			if (position<0) 
+			{
+				position =0;
+			}
+			System.out.println("Position :"+position);
 		}
 		System.out.println("position: "+position);
-		
-		
 	}
-	
-}
+
+	}
+
--- Variables.java
+++ Variables.java
@@ -2,33 +2,33 @@ package com.bl.Assignment;
 
 public class Variables {
 
-//	static int a = 10;
-//	int b = 20;
-//
-//	void i1 ()
-//	{
-//		int c = 30;
-//		int d = 40;
-//		System.out.println(c);
-//		System.out.println(d);
-//	}
-//	static void s1 ()
-//	{
-//		int e = 50;
-//		int f = 60;
-//		System.out.println(e);
-//		System.out.println(f);
-//	}
-//	public static void main(String[] args) {
-//		Variables variables = new Variables();
-//		System.out.println(a);
-//		System.out.println(variables.b);
-//		variables.i1();
-//	    Variables.s1();
-//		
-//		
-//	}
-//	
+	static int a = 10;
+	int b = 20;
+
+	void i1 ()
+	{
+		int c = 30;
+		int d = 40;
+		System.out.println(c);
+		System.out.println(d);
+	}
+	static void s1 ()
+	{
+		int e = 50;
+		int f = 60;
+		System.out.println(e);
+		System.out.println(f);
+	}
+	public static void main(String[] args) {
+		Variables variables = new Variables();
+		System.out.println(a);
+		System.out.println(variables.b);
+		variables.i1();
+	    Variables.s1();
+		
+		
+	}
+	
 	
 	
 	
