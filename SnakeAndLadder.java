6bcb631499a8e0cf940181c5de1cdebbc6a50948
Saikat7827 <saikatchakraborty957.sc@gmail.com>
2022-09-04 15:53:37 +0530

    Repeat till player reach 100

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -4,66 +4,71 @@ import java.util.Random;
 
 public class SnakeAndLadder {
 	static final int Snake = 2;
-	static final int ladder = 1;
 	static final int Ladder = 1;
 	static final int Noplay = 0;
-
-	public static void main(String[] args) 
-	{
+	
+	
+	public static void main(String[] args) {
+		
 		int position = 0;
-		Random ran = new Random();
-		int dice = ran.nextInt(6)+1;
-		System.out.println("dice: "+dice);
 		System.out.println("Initial Position is "+position);
+		System.out.println("position: "+position);
 
-		int optionCheck = ran.nextInt(3);
-		System.out.println("optionCheck: "+optionCheck);
-
-		if (optionCheck == ladder) 
 		while (position<100) 
 		{
-			System.out.println("Ladder");
-			position = position + dice;
-		}
-
-		else if (optionCheck == Snake && position > dice) 
-		{
-			System.out.println("Snake");
-			position = position - dice;
-		}
-		else {
-			System.out.println("No Play");
-			Random ran1 = new Random();
-
-			int dice1 = ran1.nextInt(6)+1;
-			System.out.println("Dice Outcome "+dice1);
-
-			int optionCheck1 = ran1.nextInt(3);
-			System.out.println("optionCheck: "+optionCheck1);
-
-
-			if (optionCheck1 == Ladder) 
-			{
-				System.out.println("Ladder");
-				position = position + dice1;
-			}
-			else if (optionCheck1 == Snake) 
+			Random ran = new Random();
+			int dice = ran.nextInt(6)+1;
+			System.out.println("Dice outcome "+dice);
+			System.out.println("dice: "+dice);
+			
+			int optionCheck = ran.nextInt(3);
+			System.out.println("optionCheck: "+optionCheck);
+			
+			if (optionCheck == Ladder) 
+				if ((optionCheck == Ladder) && (position+dice)<=100) 
+				{
+		System.out.println("Ladder");
+		position = position + dice;
+				}
+			else if (optionCheck == Snake)
 			{
 				System.out.println("Snake");
-				position = position - dice1;
+				position = position - dice;
+				
 			}
-			else 
-			{
+			else {
+				System.out.println("NoPlay");
 				System.out.println("NoPlay");
 			}
 			if (position<0) 
 			{
-				position =0;
+				System.out.println("Position: "+position);
+				System.out.println("position: "+position);
+				
+				
 			}
-			System.out.println("Position :"+position);
-		}
-		System.out.println("position: "+position);
+		
+		
+		
+		
+		
+		
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}
 
-	}
 
